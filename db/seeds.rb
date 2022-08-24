e978a16fb1e362938c2fb4df08d6fced687d2ded
Adrian Hards <github@ahards.com>
2022-08-24 09:10:54 +0100

    add time and date to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@ puts 'creating 5 tasks'
 Task.create([
               { name: 'Buy Groceries', date: Time.new(2022, 8, 28), time: '08:00' },
 
-              { name: 'Play tennis', date: Time.new(2022, 8, 28), time: Time.new(2022, 8, 28, 8, 0, 0).strftime('%H:%M') },
+              { name: 'Play tennis', date: Time.new(2022, 8, 28), time: '08:00' },
 
-              { name: 'Do the Laundry', date: Time.new(2022, 8, 28), time: Time.new(2022, 8, 28, 8, 0, 0).strftime('%H:%M') },
+              { name: 'Do the Laundry', date: Time.new(2022, 8, 28), time: '08:00' },
 
-              { name: 'Use nesting with Turbo', date: Time.new(2022, 8, 28), time: Time.new(2022, 8, 28, 8, 0, 0).strftime('%H:%M') },
+              { name: 'Use nesting with Turbo', date: Time.new(2022, 8, 28), time: '08:00' },
 
-              { name: 'Pay rent', date: Time.new(2022, 8, 28), time: Time.new(2022, 8, 28, 8, 0, 0).strftime('%H:%M') }
+              { name: 'Pay rent', date: Time.new(2022, 8, 28), time: '08:00' }
 
             ])
 
