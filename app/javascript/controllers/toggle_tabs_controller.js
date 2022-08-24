5f7a2f86256396c451103bf78eef5905fbb6c257
Adrian Hards <github@ahards.com>
2022-08-24 12:15:43 +0100

    hard replace bg colour of tabs when selected

--- app/javascript/controllers/toggle_tabs_controller.js
+++ app/javascript/controllers/toggle_tabs_controller.js
@@ -4,18 +4,20 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   today(event) {
     if (event.currentTarget.nextElementSibling.classList.contains('bg-[#0279FF]')) {
-      event.currentTarget.previousElementSibling.classList.replace("bg-[#0279FF]", "bg-white")
+      event.currentTarget.nextElementSibling.classList.replace('bg-[#0279FF]', 'bg-white')
+      event.currentTarget.classList.replace('bg-white', 'bg-[#0279FF]')
     }
-    event.currentTarget.classList.toggle("bg-[#0279FF]")
-    event.currentTarget.classList.toggle("bg-white")
+    event.currentTarget.classList.replace('bg-white', 'bg-[#0279FF]')
   }
 
   all(event) {
     if (event.currentTarget.previousElementSibling.classList.contains('bg-[#0279FF]')) {
       event.currentTarget.previousElementSibling.classList.replace("bg-[#0279FF]", "bg-white")
     }
-    event.currentTarget.classList.toggle("bg-[#0279FF]")
-    event.currentTarget.classList.toggle("bg-white")
+
+    if (event.currentTarget.classList.contains('bg-white')) {
+      event.currentTarget.classList.replace("bg-white", "bg-[#0279FF]")
+    }
 
     if (event.currentTarget.classList.contains('bg-[#0279FF]')) {
       document.querySelector('#query').value = ''
