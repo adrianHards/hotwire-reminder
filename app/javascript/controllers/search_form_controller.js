bf7d56c10b6c2266411e2898934e04314e7ec4b1
Adrian Hards <github@ahards.com>
2022-08-25 07:27:19 +0100

    fixed height issues with tabs

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -3,10 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search-form"
 export default class extends Controller {
   search() {
+    // replace with static target instead?
 
-    // replace with static target?
+    // find tabs from DOM
     let tabs = document.querySelector('.tabs').getElementsByClassName('tab')
-
+    // iterate over each tab, if any are selected, de-select them on submission of new search
     for (let tab of tabs) {
       if (tab.classList.contains("bg-[#0279FF]")) {
         tab.classList.replace("bg-[#0279FF]", 'bg-white')
--- app/javascript/controllers/toggle_tabs_controller.js
+++ app/javascript/controllers/toggle_tabs_controller.js
@@ -14,14 +14,15 @@ export default class extends Controller {
   }
 
   all(event) {
+    // if the other tab has been selected, de-select it first
     if (event.currentTarget.previousElementSibling.classList.contains('bg-[#0279FF]')) {
       event.currentTarget.previousElementSibling.classList.replace("bg-[#0279FF]", "bg-white");
     }
-
+    // if the tab is hasn't been selected, change its color, else don't do anything
     if (event.currentTarget.classList.contains('bg-white')) {
       event.currentTarget.classList.replace("bg-white", "bg-[#0279FF]");
     }
-
+    // create an empty submission which will return all tasks
     if (event.currentTarget.classList.contains('bg-[#0279FF]')) {
       document.querySelector('#query').value = '';
       document.querySelector('form').requestSubmit();
