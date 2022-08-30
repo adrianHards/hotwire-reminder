b6f19eaada9e119cdb510bf5a3380807bc50b72f
Adrian <github@ahards.com>
2022-08-30 12:11:10 +0100

    set limit to char length for task name

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Task.create([
 
               { name: 'TA at Le Wagon', date: Time.new(2022, 9, 16), time: '07:00' },
 
-              { name: 'Pay rent', date: Time.new(2022, 10, 01) }
+              { name: 'Pay rent', date: Time.new(2022, 10, 01), time: '00:00' }
 
             ])
 
