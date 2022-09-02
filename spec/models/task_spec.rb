33011af79b6a6947cf7b893ec984908ddb9856d6
Adrian <github@ahards.com>
2022-09-02 16:34:51 +0100

    add name length to rspec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,8 +5,13 @@ RSpec.describe Task, :type => :model do
   subject { described_class.new }
 
   it "is valid with valid attributes" do
-    subject.name = "Anything"
+    subject.name = "tasks"
     expect(subject).to be_valid
+  end
+
+  it "is not valid if task name is shorter than 5 chars" do
+    task = Task.new(name: 'task')
+    expect(task).to_not be_valid
   end
 
   it "is not valid without a name" do
