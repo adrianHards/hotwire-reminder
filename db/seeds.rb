ed84ea27cbda59179e5d1b07d72dda9e2643a7c7
Adrian <github@ahards.com>
2022-09-02 10:54:03 +0100

    remove category create from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
 puts 'creating 1 category'
 
-all = Category.create({name: 'All'})
+# all = Category.create({name: 'All'})
 
-puts "created #{Category.count} category"
+# puts "created #{Category.count} category"
 
 puts 'creating 7 tasks'
 
 Task.create([
-              { name: 'Buy Groceries', date: Time.new(2022, 8, 28), time: '08:00', category: all },
+              { name: 'Buy Groceries', date: Time.new(2022, 8, 28), time: '08:00' },
 
-              { name: 'Play tennis', date: Time.new(2022, 10, 01), time: '10:00', category: all },
+              { name: 'Play tennis', date: Time.new(2022, 10, 01), time: '10:00' },
 
-              { name: 'Do the Laundry', date: Time.new(2022, 8, 30), time: '10:00', category: all },
+              { name: 'Do the Laundry', date: Time.new(2022, 8, 30), time: '10:00' },
 
-              { name: 'Practice Hotwire', date: Time.new(2022, 8, 25), time: '12:00', status: 1, category: all },
+              { name: 'Practice Hotwire', date: Time.new(2022, 8, 25), time: '12:00', status: 1 },
 
-              { name: 'Pay rent', date: Time.new(2022, 8, 01), time: '07:00', status: 1, category: all },
+              { name: 'Pay rent', date: Time.new(2022, 8, 01), time: '07:00', status: 1 },
 
-              { name: 'TA at Le Wagon', date: Time.new(2022, 9, 16), time: '07:00', category: all },
+              { name: 'TA at Le Wagon', date: Time.new(2022, 9, 16), time: '07:00' },
 
-              { name: 'Pay rent', date: Time.new(2022, 10, 01), time: '00:00', category: all }
+              { name: 'Pay rent', date: Time.new(2022, 10, 01), time: '00:00' }
 
             ])
 
